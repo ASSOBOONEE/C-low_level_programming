ac1100cbadfef2a53c69001c66395a4fbfd2a5c2
assobuni <amodlasisi001@gmail.com>
2025-08-26 17:52:55 +0100

    i answered my assignment question in this file

---
+++ day_1/intro-to-c.c
@@ -0,0 +1,3 @@
+we need programming language instead of 0 and 1 because it's stressful and prone to error
+
+B. there are a lot reasons engineers learn c and part of it is the speed, accessibility ,light weight and learning other languages becomes easier
