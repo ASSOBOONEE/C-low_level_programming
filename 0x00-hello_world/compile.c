e354911c8de1c6a3e3c250ba1128eff471e68b16
assobuni <amodlasisi001@gmail.com>
2025-09-03 19:05:43 +0100

    i change folder name from day_1 to 0x00-hello_world

---
+++ 0x00-hello_world/compile.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main (void) {
+	printf ("C is powerful\n");
+	return 0;
+}
---
+++ 0x00-hello_world/intro-to-c.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void) {
+	printf("my name is Lasisi Amod Olawunmi\n");
+	printf ("i'm an electrical and electronics engineering student\n");
+	return 0;
+}
